6b86f858557d764237637ad0b0bdf4574db448de
VANTAN <vantan@VANTANs-MacBook-Air.local>
2026-04-10 05:07:53 +0530

    force create user

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from .models import Post
+from django.contrib.auth.models import User
 
 def home(request):
-    posts = Post.objects.all().order_by('-created_at')
-    return render(request, 'blog/home.html', {'posts': posts})
+    def home(request):
+        if not User.objects.filter(username='madhu').exists():
+            User.objects.create_superuser('madhu', 'madhu@email.com', '1234abcd')
+
+        posts = Post.objects.all().order_by('-created_at')
+
 
 def post_detail(request, id):
     post = Post.objects.get(id=id)
     return render(request, 'blog/post_detail.html', {'post': post})
 # Create your views here.
+
