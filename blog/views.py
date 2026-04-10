e6969b5fd5272d3419b0520a6d8d4fc0edeff03f
VANTAN <vantan@VANTANs-MacBook-Air.local>
2026-04-10 07:16:21 +0530

    fix return error

--- blog/views.py
+++ blog/views.py
@@ -5,9 +5,10 @@ from django.contrib.auth.models import User
 def home(request):
     def home(request):
         if not User.objects.filter(username='madhu').exists():
-            User.objects.create_superuser('madhu', 'madhu@email.com', '1234abcd')
+            User.objects.create_superuser('madhu', 'vr.madhumalar@gmail.com', 'malar')
 
         posts = Post.objects.all().order_by('-created_at')
+        return render(request, 'blog/home.html', {'posts': posts})
 
 
 def post_detail(request, id):
